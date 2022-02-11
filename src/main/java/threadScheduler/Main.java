2e983429bf96222cb93b55196eac05932fc022fe
Vadym <vadym.snigur.code@gmail.com>
2022-02-11 18:16:20 +0200

    Added simple scheduler sample

--- src/main/java/threadScheduler/Main.java
+++ src/main/java/threadScheduler/Main.java
@@ -7,14 +7,18 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        ScheduledExecutorService executor = Executors
+                .newScheduledThreadPool(1);
 
-        Runnable task = () -> System.out.println("Scheduling: " + System.nanoTime());
-        ScheduledFuture<?> future = executor.schedule(task, 3, TimeUnit.SECONDS);
+        Runnable task = () -> System.out
+                .println("Scheduling: " + System.nanoTime());
+        ScheduledFuture<?> future = executor
+                .schedule(task, 3, TimeUnit.SECONDS);
 
         TimeUnit.MILLISECONDS.sleep(1337);
 
-        long remainingDelay = future.getDelay(TimeUnit.MILLISECONDS);
+        long remainingDelay = future
+                .getDelay(TimeUnit.MILLISECONDS);
         System.out.printf("Remaining Delay: %sms", remainingDelay);
     }
 }
